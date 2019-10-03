71dbe674ab266e8be2346dd4d1fcafc78b8c9bf9
Nickshajtan <shajtanuch@gmail.com>
2019-10-03 16:15:56 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -38,3 +38,13 @@ jQuery(document).ready(function($) {
         $('.menu').toggleClass('d-flex');
     });
 });
+function validate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
